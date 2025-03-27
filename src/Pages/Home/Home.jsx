1badd941af676626b064d951562d57816ec2c61d
YatinDevs <c.yatin727@gmail.com>
2025-03-27 11:10:05 +0530

    Updates: VideoSection

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import HeroSection from "./HeroSection";
 import CTA from "./CTA";
 import ServicesSection from "./AboutService";
 import AssetManagementSection from "./AssetManagement";
+import VideoSection from "./VideoSection";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <ServicesSection />
+      <VideoSection />
       <AssetManagementSection />
       <CTA />
     </div>
